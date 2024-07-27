3952ecb0b5e8a27b13ce71973c8865f06762baef
“Luis” <mrqueiroz22014@gmail.com>
2024-07-27 16:09:11 -0300

    Adding build and deploy .py

---
+++ pipeline/build.py
@@ -0,0 +1,49 @@
+import kfp
+import sys
+import os
+from kfp.dsl import pipeline, Input, Output, Dataset, Model, Metrics
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from components.download_data import upload_dataset
+from components.preprocess import preprocess_data
+from components.training import train_model
+from components.evaluate_model import evaluate_model
+
+@pipeline(
+    name='Machine Learning Pipeline',
+    description='An example pipeline that performs data download, preprocessing, training, and evaluation.'
+)
+def ml_pipeline(
+    query: str,
+    project_id: str,
+    train_size: float,
+    test_size: float,
+    valid_size: float,
+    target_column_name: str,
+    deployment_metric: str,
+    deployment_metric_threshold: float
+):
+    download_task = upload_dataset(query=query, project_id=project_id)
+
+    preprocess_task = preprocess_data(
+        train_size=train_size,
+        test_size=test_size,
+        valid_size=valid_size,
+        input_data=download_task.outputs['df']
+    )
+
+    train_task = train_model(
+        train_data=preprocess_task.outputs['train_data']
+    )
+
+    evaluate_task = evaluate_model(
+        test_data=preprocess_task.outputs['test_data'],
+        model=train_task.outputs['model'],
+        target_column_name=target_column_name,
+        deployment_metric=deployment_metric,
+        deployment_metric_threshold=deployment_metric_threshold
+    )
+
+if __name__ == '__main__':
+    kfp.compiler.Compiler().compile(ml_pipeline, 'ml_pipeline.yaml')
---
+++ pipeline/deploy.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from google.cloud import aiplatform
+from utils.globals import PIPELINE_ROOT, QUERY
+
+aiplatform.init(
+    project='annular-weaver-428312-s3',
+    location='us-central1'  
+)
+
+job = aiplatform.PipelineJob(
+    display_name="intro_pipeline",
+    template_path="ml_pipeline.yaml",
+    pipeline_root=PIPELINE_ROOT,
+    parameter_values={
+        "query": QUERY,
+        "project_id": "annular-weaver-428312-s3",
+        "train_size": 0.6,
+        "test_size": 0.2,
+        "valid_size": 0.2,
+        "target_column_name": "cnt",
+        "deployment_metric": "r2",
+        "deployment_metric_threshold": 0.7
+    },
+)
+
+job.run()
