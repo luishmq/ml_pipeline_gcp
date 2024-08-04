80988d9e4b153c3a974f3531fe7ddf628dac7cbb
“Luis” <mrqueiroz22014@gmail.com>
2024-08-04 14:34:59 -0300

    Changing pipeline components and files structure

--- pipeline/build.py
+++ pipeline/build.py
@@ -6,44 +6,30 @@ from kfp.dsl import pipeline
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from components.download_data import upload_dataset
-from components.preprocess import preprocess_data
-from components.training import train_model
+from components.training import training
 from components.evaluate_model import evaluate_model
 
 @pipeline(
     name='Machine Learning Pipeline',
-    description='An example pipeline that performs data download, preprocessing, training, and evaluation.'
+    description='Pipeline que extrai dados do Cloud Storage, treina um modelo de regressão e avalia o modelo treinado.'
 )
 def ml_pipeline(
-    query: str,
-    project_id: str,
-    train_size: float,
-    test_size: float,
-    valid_size: float,
-    target_column_name: str,
-    deployment_metric: str,
-    deployment_metric_threshold: float
+    bucket_name: str,
+    source_blob_name: str,
 ):
-    download_task = upload_dataset(query=query, project_id=project_id)
+    download_task = upload_dataset(
+        bucket_name=bucket_name,
+        source_blob_name=source_blob_name
+    )
 
-    preprocess_task = preprocess_data(
-        train_size=train_size,
-        test_size=test_size,
-        valid_size=valid_size,
+    training_task = training(
         input_data=download_task.outputs['df']
     )
 
-    train_task = train_model(
-        train_data=preprocess_task.outputs['train_data']
-    )
-
     evaluate_model(
-        test_data=preprocess_task.outputs['test_data'],
-        model=train_task.outputs['model'],
-        target_column_name=target_column_name,
-        deployment_metric=deployment_metric,
-        deployment_metric_threshold=deployment_metric_threshold
+        input_data=download_task.outputs['df'],
+        trained_model=training_task.outputs['trained_model']
     )
 
 if __name__ == '__main__':
-    kfp.compiler.Compiler().compile(ml_pipeline, 'ml_pipeline.yaml')
+    kfp.compiler.Compiler().compile(ml_pipeline, 'yaml/ml_pipeline.yaml')
--- pipeline/deploy.py
+++ pipeline/deploy.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from google.cloud import aiplatform
-from utils.globals import PIPELINE_ROOT, QUERY
+from utils.globals import PIPELINE_ROOT
 
 aiplatform.init(
     project='annular-weaver-428312-s3',
@@ -12,18 +12,12 @@ aiplatform.init(
 )
 
 job = aiplatform.PipelineJob(
-    display_name="intro_pipeline",
-    template_path="ml_pipeline.yaml",
+    display_name="ml_pipeline",
+    template_path="yaml/ml_pipeline.yaml",
     pipeline_root=PIPELINE_ROOT,
     parameter_values={
-        "query": QUERY,
-        "project_id": "annular-weaver-428312-s3",
-        "train_size": 0.6,
-        "test_size": 0.2,
-        "valid_size": 0.2,
-        "target_column_name": "cnt",
-        "deployment_metric": "r2",
-        "deployment_metric_threshold": 0.7
+        "bucket_name": "data_pipeline_paris",
+        "source_blob_name": "ParisHousing.csv",
     },
 )
 
