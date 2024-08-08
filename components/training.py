809ff7e695e57ea82c3129702f2bbede21f5cc80
“Luis” <mrqueiroz22014@gmail.com>
2024-08-08 18:50:26 -0300

    Pequenas alterações

--- components/training.py
+++ components/training.py
@@ -1,6 +1,6 @@
 from kfp.dsl import component, Output, Dataset, Input, Model
 
-@component(base_image="python:3.11", output_component_file="../components/yaml/preprocess_data.yaml", packages_to_install=["pandas", "pycaret"])
+@component(base_image="python:3.11", output_component_file="../components/yaml/training.yaml", packages_to_install=["pandas", "pycaret"])
 def training(train_data: Input[Dataset], trained_model: Output[Model]):
     import pandas as pd
     from pycaret.regression import setup, tune_model, save_model, compare_models
